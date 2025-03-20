3e307bcbfae99259ffa2acaf4bfaa4d2409fac9b
Assis Sales <francisco.assis@enorsul.com.br>
2025-03-20 13:40:29 -0300

    Atualização da rotina
    Rotina para automatizar

--- atualizar_git.bat
+++ atualizar_git.bat
@@ -1,5 +1,13 @@
 @echo off
 cd /d C:\Users\franc\OneDrive\Documentos\GitHub\Dash_Guarulhos
+
+:: Inicia o Streamlit sem fechar o terminal
 start "" "C:\Users\franc\OneDrive\Documentos\GitHub\Dash_Guarulhos\.venv\Scripts\python.exe" -m streamlit run test_jortform.py
-start "" "C:\Users\franc\OneDrive\Documentos\GitHub\Dash_Guarulhos\.venv\Scripts\python.exe" atualizar_git.py
+
+:: Aguarda 5 segundos para garantir que o Streamlit iniciou
+timeout /t 5 /nobreak
+
+:: Atualiza o repositório Git
+"C:\Users\franc\OneDrive\Documentos\GitHub\Dash_Guarulhos\.venv\Scripts\python.exe" atualizar_git.py
+
 exit
