f5552960790e79ec156e40cb66f5568db4a611c6
Assis Sales <francisco.assis@enorsul.com.br>
2025-03-22 00:57:05 -0300

    atualização
    atualização atual

--- atualizar_streamlit.bat
+++ atualizar_streamlit.bat
@@ -1,10 +1,13 @@
 @echo off
-cd /d C:\Users\franc\OneDrive\Documentos\ASSIS\vu_guarulhos_28022025\Dash_Guarulhos\Dash_Guarulhos
+
 echo Finalizando processos antigos...
 tasklist | find /I "streamlit.exe" && taskkill /F /IM streamlit.exe /T
 tasklist | find /I "python.exe" && taskkill /F /IM python.exe /T
+
 echo Atualizando repositório...
 git pull origin main
+
 echo Iniciando Streamlit...
-start "" python -m streamlit run test_jortform.py --server.port 8501
+start "" C:\Users\franc\OneDrive\Documentos\ASSIS\vu_guarulhos_28022025\Dash_Guarulhos\Dash_Guarulhos\.venv\Scripts\python.exe -m streamlit run test_jortform.py --server.port 8501
+
 exit
